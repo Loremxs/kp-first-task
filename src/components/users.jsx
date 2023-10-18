82049afbe16bd23a654c5d4ead99e14611e44163
Loremxs <burkeev.valera03@mail.ru>
2023-10-19 01:15:43 +0300

    first task kp

---
+++ src/components/users.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import api from "../api";
+
+const Users = () => {
+  const [users, setUsers] = useState(api.users.fetchAll());
+  const renderPhrase = (number) => {
+    const lastOne = Number(number.toString().slice(-1));
+    console.log(lastOne);
+    if ((number > 4 && number < 15) || number === 1) {
+      return "Человек тусанет";
+    } else if ([2, 3, 4].indexOf(lastOne) >= 0) {
+      return "Человека тусанет";
+    }
+  };
+  const handleDelete = (userId) => {
+    // setUsers(users.filter((user) => user._id !== userId));
+    setUsers((prevState) => prevState.filter((user) => user._id !== userId));
+  };
+  return (
+    <>
+      <h2>
+        <span
+          className={"badge bg-" + (users.length > 0 ? "primary" : "danger")}
+        >
+          {users.length > 0
+            ? `${users.length} ${renderPhrase(users.length)} с тобой сегодня`
+            : "Никто с тобой не тусанет"}
+        </span>
+      </h2>
+      {users.length > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встречи</th>
+              <th scope="col">Оценка</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>
+                  {user.qualities.map((quality) => (
+                    <span
+                      className={"badge m-2 bg-" + quality.color}
+                      key={quality._id}
+                    >
+                      {quality.name}
+                    </span>
+                  ))}
+                </td>
+                <td>{user.profession.name}</td>
+                <td>{user.completedMeetings}</td>
+                <td>{user.rate}</td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(user._id)}
+                  >
+                    Удалить
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
+};
+
+export default Users;
